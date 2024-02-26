b5a906cc0fb518bb0906f2e4f98d41d842b12c37
crinkj <jcrinkey89120@gmail.com>
2024-02-26 15:16:50 +0900

    chore: h2 db 연동 & jpa 기본 세팅

---
+++ src/main/kotlin/laboratory/experiment/ExperimentApplication.kt
@@ -0,0 +1,11 @@
+package laboratory.experiment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExperimentApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExperimentApplication>(*args)
+}
